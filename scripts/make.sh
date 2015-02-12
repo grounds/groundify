109e034eaea8db3d10131f98ce4d6196c791df71
Adrien Folie <folie.adrien@gmail.com>
2015-02-12 15:59:20 +0100

    add test command in script/make.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -30,10 +30,14 @@ minify() {
     uglifyjs $build_target -o $min_target
 }
 
+test() {
+    mocha-phantomjs test/gist.html
+}
+
 main() {
     # If first parameter from CLI is missing or empty
     if [ -z $1 ]; then
-        echo "usage: build [dependencies|dev|build|minify]"
+        echo "usage: build [dependencies|dev|build|minify|test]"
         return
     fi
     eval $1
