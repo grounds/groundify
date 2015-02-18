7818357454475b20dc34de8e13727b0c0216ab83
Adrien Folie <folie.adrien@gmail.com>
2015-02-19 00:36:45 +0100

    use npm modules binaries

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,16 +2,13 @@
 set -e
 
 # Binaries used to build, test and package this project.
-browserify="./node_modules/browserify/bin/cmd.js"
-watchify="./node_modules/watchify/bin/cmd.js"
-uglify="./node_modules/uglify-js/bin/uglify"
-mocha_phantomjs="./node_modules/mocha-phantomjs/bin/mocha-phantomjs"
+bin="./node_modules/.bin"
+
+project="groundify"
 
 origin="index.js"
-project="groundify"
 build_target="build/$project.js"
 build_min_target="build/$project.min.js"
-
 
 clean() {
     rm -rf build/*
@@ -22,19 +19,19 @@ dependencies() {
 }
 
 dev() {
-    $watchify $origin -o $build_target
+    $bin/watchify $origin -o $build_target
 }
 
 build() {
-    $browserify $origin -o $build_target
+    $bin/browserify $origin -o $build_target
 }
 
 minify() {
-    $uglifyjs $build_target -o $build_min_target
+    $bin/uglifyjs $build_target -o $build_min_target
 }
 
 test_acceptance() {
-    $mocha_phantomjs test/SpecRunner.html
+    $bin/mocha_phantomjs test/SpecRunner.html
 }
 
 test_unit() {
